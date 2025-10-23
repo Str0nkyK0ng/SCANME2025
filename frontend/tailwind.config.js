afff26827cf2a5b3ad9ec5ceef1894299518bf5d
Aidan Strong <aidan.m.strong@gmail.com>
2025-10-23 12:03:59 -0700

    vercel json

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,25 +8,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        fluxisch: ['Fluxisch Else', 'monospace'],
-      },
-      extend: {
-        keyframes: {
-          'flash-red': {
-            '0%': { backgroundColor: 'rgba(255,0,0,0.7)' },
-            '70%': { backgroundColor: 'rgba(255,0,0,0.7)' },
-            '100%': { backgroundColor: 'rgba(255,0,0,0)' },
-          },
-          'flash-green': {
-            '0%': { backgroundColor: 'rgba(0,255,0,0.7)' },
-            '70%': { backgroundColor: 'rgba(0,255,0,0.7)' },
-            '100%': { backgroundColor: 'rgba(0,255,0,0)' },
-          },
-        },
-        animation: {
-          'flash-red': 'flash-red 0.6s ease',
-          'flash-green': 'flash-green 0.6s ease',
-        },
+        fluxisch: ['Fluxisch Else'],
       },
     },
   },
